4fc502ce4bdce90637b962b0c260745415927e33
aakashsadnani <A00235504@mycambrian.ca>
2021-08-13 11:30:16 -0400

    Fixing of dicehistory and added comments in MainActivity file

--- app/src/main/java/com/aakash/dicerolling/MainActivity.java
+++ app/src/main/java/com/aakash/dicerolling/MainActivity.java
@@ -36,80 +36,68 @@ import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
-
-
+    //initialization of various widgets here
     Spinner spin;
     Button rollOneDiceButton;
-    String diceResult, diceResult2;
+    String diceResult;
     int maxValue = 4;
-    TextView diceResult1Textview, diceResult2Textview, historyDiceTextView, historyTitleDice2RollTextView, historyDice2TextView, restoreAllSidesTextView, clearListTextView;
+    TextView diceResult1Textview, diceResult2Textview, historyDiceTextView, restoreAllSidesTextView, clearListTextView;
     EditText customDiceSidesEditText;
     LinearLayout secondDiceLayoutView, customSidesLayout;
     RadioGroup radioGroup;
     RadioButton oneDiceRadioButton, twoDiceRadioButton, customDiceRadioButton;
     ArrayList<String> arrayList = new ArrayList<>();
     ArrayList<String> arrayListHistory = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //SharedPreferences sharedPreferences = SharedPreferences
-
-
+        //calling the get ids and getdata if any available
         getID();
         getData();
+
+        //setting on click listners
         rollOneDiceButton.setOnClickListener(this);
         clearListTextView.setOnClickListener(this);
+        restoreAllSidesTextView.setOnClickListener(this);
+        spin.setOnItemSelectedListener(this);
 
+        //setting visibility of seconf dice view to gone
         secondDiceLayoutView.setVisibility(View.GONE);
         customSidesLayout.setVisibility(View.GONE);
-        spin.setOnItemSelectedListener(this);
-        restoreAllSidesTextView.setOnClickListener(this);
 
-        historyTitleDice2RollTextView.setVisibility(View.GONE);
-        historyDice2TextView.setVisibility(View.GONE);
-
+        //defining arrayadapter for spinner
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        //Creating the ArrayAdapter instance having the country list
-        //ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sides);
-        //aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
+        //we can set the adapter here
         spin.setAdapter(arrayAdapter);
 
+        //check for any text in custom edit text field and set the text in textedit and spinner option selection
         if(customDiceSidesEditText.getText() != null && arrayList.contains(customDiceSidesEditText.getText().toString()) == true){
-            int kk = arrayList.indexOf(customDiceSidesEditText.getText().toString());
-            spin.setSelection(kk);
+            int gettext = arrayList.indexOf(customDiceSidesEditText.getText().toString());
+            spin.setSelection(gettext);
         }
+
+        //radio group onclick check listner for checking the radio button selected to do a task
+        //based on selections views will change
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch (checkedId) {
                     case R.id.oneDiceRadioButton:
-
                         secondDiceLayoutView.setVisibility(View.GONE);
                         customSidesLayout.setVisibility(View.GONE);
-                        historyTitleDice2RollTextView.setVisibility(View.GONE);
-                        historyDice2TextView.setVisibility(View.GONE);
-
                         break;
                     case R.id.twoDiceRadioButton:
                         secondDiceLayoutView.setVisibility(View.VISIBLE);
                         customSidesLayout.setVisibility(View.GONE);
-
-                        historyTitleDice2RollTextView.setVisibility(View.VISIBLE);
-                        historyDice2TextView.setVisibility(View.VISIBLE);
-
                         break;
                     case R.id.customDiceRadioButton:
                         secondDiceLayoutView.setVisibility(View.GONE);
                         customSidesLayout.setVisibility(View.VISIBLE);
-
-                        historyTitleDice2RollTextView.setVisibility(View.GONE);
-                        historyDice2TextView.setVisibility(View.GONE);
-
                         break;
 
 
@@ -123,16 +111,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
         Object item = arg0.getItemAtPosition(position);
-        Toast.makeText(getApplicationContext(), item.toString(), Toast.LENGTH_SHORT).show();
         if(item.toString().contentEquals("10 sides")){
-            Toast.makeText(getApplicationContext(), "Side 10 Custom", Toast.LENGTH_LONG).show();
             maxValue = new Dice(10).numberOfSides;
         }
         else if(item.toString().contentEquals("10 sides 0-9")){
             maxValue = new Dice(10).numberOfSides -1;
         }
         else {
-
             maxValue = new Dice(Integer.parseInt(item.toString())).numberOfSides;
         }
     }
@@ -142,42 +127,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // TODO Auto-generated method stub
     }
 
+    //here all the Ids are defined and called at the oncreate
     public void getID() {
         spin = findViewById(R.id.selectDiceSpinner);
         rollOneDiceButton = findViewById(R.id.rollOneDiceButton);
-
         diceResult1Textview = findViewById(R.id.diceResult1Textview);
         diceResult2Textview = findViewById(R.id.diceResult2Textview);
-
-
         secondDiceLayoutView = findViewById(R.id.secondDiceLayoutView);
-
         radioGroup = findViewById(R.id.selectSidesRadioGroup);
         oneDiceRadioButton = findViewById(R.id.oneDiceRadioButton);
         twoDiceRadioButton = findViewById(R.id.twoDiceRadioButton);
         customDiceRadioButton = findViewById(R.id.customDiceRadioButton);
-
         customSidesLayout = findViewById(R.id.customSidesLayout);
         customDiceSidesEditText = findViewById(R.id.customSidesEditText);
-
-
         historyDiceTextView = findViewById(R.id.historyDiceTextView);
-
-        historyTitleDice2RollTextView = findViewById(R.id.historyTitleDice2RollTextView);
-        historyDice2TextView = findViewById(R.id.historyDice2TextView);
-
         restoreAllSidesTextView = findViewById(R.id.restoreAllSidesTextView);
-
         clearListTextView = findViewById(R.id.clearListTextView);
     }
 
+    //the onclick listner click is defined here by the button id we can check what button is clicked
     @SuppressLint("NonConstantResourceId")
     public void onClick(View view) {
         switch (view.getId()) {
 
             case R.id.rollOneDiceButton:
-                //saveData();
-
                 if (oneDiceRadioButton.isChecked()) {
                     if(spin.getSelectedItem() == "Custom 10 sides"){
                         diceResult = String.valueOf(new Dice(maxValue).rollmethod() * 10);
@@ -186,18 +159,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         diceResult = String.valueOf(new Dice(maxValue).rollmethod());
                     }
                     diceResult1Textview.setText(diceResult);
-
                     arrayListHistory.add(diceResult);
-//                    saveData();
-//                    getData();
-                    getArrayListdata();
+                    setArrayListdata();
+
                 } else if (twoDiceRadioButton.isChecked()) {
                     diceResult = String.valueOf(new Dice(maxValue).rollmethod());
                     diceResult1Textview.setText(diceResult);
 
                     arrayListHistory.add(diceResult);
-
-                    diceResult2 = diceResult;
 
                     diceResult = String.valueOf(new Dice(maxValue).rollmethod());
 
@@ -205,24 +174,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                     arrayListHistory.add(diceResult);
 
-                    getArrayListdata();
+                    setArrayListdata();
 
                 } else if (customDiceRadioButton.isChecked()) {
+
+                    //here try catch is used as user input may cause some error and crash
                     try{
-                    int numberofsides = Integer.parseInt(String.valueOf(customDiceSidesEditText.getText()));
-                    if (numberofsides < 100) {
+
+                        int numberofsides = Integer.parseInt(String.valueOf(customDiceSidesEditText.getText()));
+
+                        if (numberofsides < 100) {
                         try {
                             diceResult = String.valueOf(new Dice(numberofsides).rollmethod());
                             diceResult1Textview.setText(diceResult);
                             if(arrayList.contains(String.valueOf(numberofsides))){
-
+                                arrayListHistory.add(diceResult);
+                                setArrayListdata();
                             }
                             else{
                             arrayList.add(String.valueOf(numberofsides));
-//                            saveData();
-//                            getData();
-                            int kk = arrayList.indexOf(String.valueOf(numberofsides));
-                            spin.setSelection(kk);
+                            int sides = arrayList.indexOf(String.valueOf(numberofsides));
+                            spin.setSelection(sides);
+                                arrayListHistory.add(diceResult);
+                                setArrayListdata();
 
                             }
                         } catch (Exception e) {
@@ -235,8 +209,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
                     }
                 }
-
                 break;
+
             case R.id.restoreAllSidesTextView :
                 Toast.makeText(getApplicationContext(), "Restoring..", Toast.LENGTH_SHORT).show();
                 arrayList.clear();
@@ -252,17 +226,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 break;
 
             case R.id.clearListTextView:
+                Toast.makeText(getApplicationContext(), "Restoring..", Toast.LENGTH_SHORT).show();
                 arrayListHistory.clear();
                 SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
                 SharedPreferences.Editor myEdit = sharedPreferences.edit();
                 myEdit.remove("historyList1");
                 myEdit.apply();
-                //saveData();
-                //getData();
-                getArrayListdata();
+                setArrayListdata();
+                diceResult = "No History";
                 break;
-                default:
 
+                default:
                 Toast.makeText(this, "View not implemented", Toast.LENGTH_SHORT).show();
                 break;
         }
@@ -281,6 +255,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    //defining a function for saving data in shared pref
     public void saveData() {
         SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         SharedPreferences.Editor myEdit = sharedPreferences.edit();
@@ -288,14 +263,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         myEdit.putString("value", customDiceSidesEditText.getText().toString());
 
 
-        if (diceResult != null) {
+        if (diceResult != null && !diceResult.contentEquals("No History")) {
             arrayListHistory.add(diceResult);
             Set<String> setHistory = new HashSet<String>();
             setHistory.addAll(arrayListHistory);
             myEdit.putStringSet("historyList1", setHistory).apply();
 
         } else {
-            myEdit.putString("history", "0");
+            myEdit.putString("historyList1", null);
         }
         myEdit.putStringSet("dicelist", null);
         Set<String> set = new HashSet<String>();
@@ -306,13 +281,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    //all the data saved in shared pref are get through this function
     public void getData() {
         SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
 
         String s1 = sh.getString("value", "0");
 
         customDiceSidesEditText.setText(s1);
-
 
         Set<String> set = sh.getStringSet("dicelist", null);
         if(set != null){
@@ -332,36 +307,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         }
 
-
         Set<String> setii = sh.getStringSet("historyList1", null);
-        List<String> sample=new ArrayList<String>(setii);
-        
-//        Set<String> setii = sh.getStringSet("historyList1", null);
         if(setii != null) {
+            List<String> sample=new ArrayList<String>(setii);
             arrayListHistory.clear();
         for(int k=0;k<sample.size();k++){
             arrayListHistory.add(sample.get(k));
             Log.d("retrivesharedPreferences", "" + setii);
-}
+    }
+            setArrayListdata();
         }
-
-
-        getArrayListdata();
     }
 
-    public void getArrayListdata(){
-        if (arrayListHistory != null) {
-
+    //here we set the arraylist data from the data we get from the historyof arraylist by the sharedpref
+    public void setArrayListdata(){
+        if (!arrayListHistory.isEmpty()) {
             historyDiceTextView.setText("");
             for(int i=0; i<arrayListHistory.size(); i++){
                 historyDiceTextView.append(arrayListHistory.get(i) + "\n");
-
-                Log.i("arraylist", arrayListHistory.get(i));
             }
-            Log.i("arraylist", "new log");
-            //historyDice2TextView.setText(getHistory2);
         } else {
-//            getHistory = "0";
+            historyDiceTextView.setText("No History!");
         }
     }
 
